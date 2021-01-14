3a0933dbe3266cc1b44dd13afd58921e1c81b912
Іван Погибко <pogybkoivan@gmail.com>
2021-01-14 22:46:55 +0200

    settings sidebar: innitial form config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SettingsSidebarComponent } from './settings-sidebar/components/settings-sidebar/settings-sidebar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSliderModule} from "@angular/material/slider";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { SettingsSidebarComponent } from './settings-sidebar/components/settings
     SettingsSidebarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/settings-sidebar/components/settings-sidebar/settings-sidebar.component.ts
+++ src/app/settings-sidebar/components/settings-sidebar/settings-sidebar.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, HostBinding, OnDestroy, OnInit, Output} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
+import {Subject} from "rxjs";
+import {takeUntil} from "rxjs/operators";
+
+export interface IPatternSettings {
+  sideA: number;
+  sideB: number;
+}
 
 @Component({
   selector: 'pg-settings-sidebar',
   templateUrl: './settings-sidebar.component.html',
   styleUrls: ['./settings-sidebar.component.scss']
 })
-export class SettingsSidebarComponent implements OnInit {
-
-  constructor() { }
+export class SettingsSidebarComponent implements OnInit, OnDestroy {
+  @Output() public settingsChange: EventEmitter<IPatternSettings> = new EventEmitter<IPatternSettings>()
+  @HostBinding('class.mat-elevation-z8') public elevation: boolean = true;
+  public settingsForm: FormGroup = new FormGroup({
+    sideA: new FormControl(2),
+    sideB: new FormControl(2),
+  });
+  private _unsubscribe$: Subject<void> = new Subject<void>();
 
   ngOnInit(): void {
+    this.settingsForm.valueChanges
+      .pipe(takeUntil(this._unsubscribe$))
+      .subscribe(value => this.settingsChange.emit(value));
   }
 
+  public ngOnDestroy(): void {
+    this._unsubscribe$.next();
+    this._unsubscribe$.complete();
+  }
 }
